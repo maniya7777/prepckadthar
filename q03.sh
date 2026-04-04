2550d0018a13f33cc5e3c0c27a81ccd60915172f
maniya7777 <89803755+maniya7777@users.noreply.github.com>
2026-04-04 21:34:59 +0530

    Update q03.sh
    hello word add

--- q03.sh
+++ q03.sh
@@ -7,12 +7,14 @@ echo "Kubernetes CKAD Practice Question"
 echo "========================================"
 echo ""
 
-echo "Task"
-echo ""
-echo "First, update the Deployment ckad00017-deployment in the namespace ckad00017: Scale it to run 3 Pod replicas. Add the label tier: dmz to the Pods."
-echo ""
-echo "Then, create a NodePort Service named rover in the namespace ckad00017 to expose the Deployment ckad00017-deployment on TCP port 81."
-echo ""
+cat <<EOF
+Task
+
+First, update the Deployment ckad00017-deployment in the namespace ckad00017: Scale it to run 3 Pod replicas and add the label tier: dmz to the Pods. 
+Then, create a NodePort Service named rover in the namespace ckad00017 to expose the Deployment ckad00017-deployment on TCP port 81. 
+When accessing NodeIP:NodePort the output should display 'Hello World'.
+
+EOF
 
 echo "========================================"
 echo "Preparing Environment..."
@@ -21,12 +23,32 @@ echo "========================================"
 # create namespace
 kubectl create namespace ckad00017
 
-# create initial deployment (not matching required state)
-kubectl create deployment ckad00017-deployment \
-  --image=nginx \
-  --replicas=1 \
-  --port=81 \
-  -n ckad00017
+# create initial deployment
+kubectl apply -f - <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ckad00017-deployment
+  namespace: ckad00017
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ckad00017
+  template:
+    metadata:
+      labels:
+        app: ckad00017
+    spec:
+      containers:
+      - name: web
+        image: busybox
+        command: ["/bin/sh","-c"]
+        args:
+        - while true; do echo -e "HTTP/1.1 200 OK\n\nHello World" | nc -l -p 81; done
+        ports:
+        - containerPort: 81
+EOF
 
 echo ""
 echo "Environment ready."
